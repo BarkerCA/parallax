6e582d14bc8d5b81604844562c0bce7aca559486
Chris Barker <chris.a.barker@icloud.com>
2014-09-12 06:23:55 -0400

    Initial Commit

---
+++ app/assets/javascripts/reftagger.js
@@ -0,0 +1,22 @@
+var refTagger = {
+  settings: {
+    bibleVersion: "NIV",      
+    roundCorners: true,     
+    customStyle : {
+      heading: {
+        color : "#77B800"
+      },
+      body   : {
+        color : "#003592",
+        moreLink : {
+          color: "#77B800"
+        }
+      }
+    }
+  }
+};
+(function(d, t) {
+  var g = d.createElement(t), s = d.getElementsByTagName(t)[0];
+  g.src = "//api.reftagger.com/v2/RefTagger.js";
+  s.parentNode.insertBefore(g, s);
+}(document, "script"));
