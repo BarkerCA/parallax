33cac65b49b5a56e4a0eda5e17bd1831a63c43ed
Chris Barker <chris.a.barker@icloud.com>
2014-12-19 20:10:58 -0500

    Small pnc-parallax updates

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,8 @@ Rails.application.routes.draw do
   get 'general' => 'test#general'
   get 'giving'  => 'giving#index'
   
+  get 'pages/about' => 'pages#about'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
